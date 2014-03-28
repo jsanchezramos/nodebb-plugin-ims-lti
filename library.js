9df11420708c699f0b703b5337eb3c9e6103b76d
Juan Francisco Sanchez <jsanchezramos@uoc.edu>
2014-03-28 12:20:53 +0100

    Changes template and library.js to adapt lms connection

--- library.js
+++ library.js
@@ -32,6 +32,7 @@
 
 	OAuth.getStrategy = function(strategies, callback) {
 		meta.settings.get('sso-oauth', function(err, settings) {
+			
 			if (err) {
 				winston.error('[plugins/sso-oauth] Could not retrieve OAuth settings: ' + err.message);
 			} else if (!settings) {
@@ -47,83 +48,43 @@
 				}),
 				opts;
 
-			if (settings['oauth:type'] === '2') {
-				passportOAuth = require('passport-oauth').OAuth2Strategy;
-			} else if (settings['oauth:type'] === '1') {
+			
 				passportOAuth = require('passport-oauth').OAuthStrategy;
-			}
+			
 
 			if (passportOAuth && configOk) {
-				if (settings['oauth:type'] === '1') {
-					// OAuth options
-					opts = {
-						requestTokenURL: settings['oauth:reqTokenUrl'],
-						accessTokenURL: settings['oauth:accessTokenUrl'],
-						userAuthorizationURL: settings['oauth:authUrl'],
-						consumerKey: settings['oauth:key'],
-						consumerSecret: settings['oauth:secret'],
-						callbackURL: nconf.get('url') + '/auth/generic/callback'
-					};
+				
+				// OAuth options
+				opts = {
+					requestTokenURL: settings['oauth:reqTokenUrl'],
+					accessTokenURL: settings['oauth:accessTokenUrl'],
+					userAuthorizationURL: settings['oauth:authUrl'],
+					consumerKey: settings['oauth:key'],
+					consumerSecret: settings['oauth:secret'],
+					callbackURL: nconf.get('url') + '/auth/generic/callback'
+				};
 
-					passportOAuth.Strategy.prototype.userProfile = function(token, secret, params, done) {
-						this._oauth.get(settings['oauth:userProfileUrl'], token, secret, function(err, body, res) {
-							if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
+				passportOAuth.Strategy.prototype.userProfile = function(token, secret, params, done) {
+					this._oauth.get(settings['oauth:userProfileUrl'], token, secret, function(err, body, res) {
+						if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
 
-							try {
-								var json = JSON.parse(body);
+						try {
+							var json = JSON.parse(body);
 
-								var profile = { provider: 'generic' };
-								// Uncomment the following lines to include whatever data is necessary
-								// NodeBB requires the following: id, displayName, emails, e.g.:
-								// profile.id = json.id;
-								// profile.displayName = json.name;
-								// profile.emails = [{ value: json.email }];
+							var profile = { provider: 'generic' };
+							// Uncomment the following lines to include whatever data is necessary
+							// NodeBB requires the following: id, displayName, emails, e.g.:
+							// profile.id = json.id;
+							// profile.displayName = json.name;
+							// profile.emails = [{ value: json.email }];
 
-								done(null, profile);
-							} catch(e) {
-								done(e);
-							}
-						});
-					};
-				} else if (settings['oauth:type'] === '2') {
-					// OAuth 2 options
-					opts = {
-						authorizationURL: settings['oauth2:authUrl'],
-						tokenURL: settings['oauth2:tokenUrl'],
-						clientID: settings['oauth2:id'],
-						clientSecret: settings['oauth2:secret'],
-						callbackURL: nconf.get('url') + '/auth/generic/callback'
-					};
-
-					passportOAuth.Strategy.prototype.userProfile = function(accessToken, done) {
-						this._oauth2.get(settings['oauth:userProfileUrl'], accessToken, function(err, body, res) {
-							if (err) { return done(new InternalOAuthError('failed to fetch user profile', err)); }
-
-							try {
-								var json = JSON.parse(body);
-
-								var profile = { provider: 'generic' };
-								// Alter this section to include whatever data is necessary
-								// NodeBB requires the following: id, displayName, emails, e.g.:
-
-								profile.id = json.id;
-								profile.displayName = json.name;
-								profile.emails = [{ value: json.email }];
-
-								// Find out what is available by uncommenting this line:
-								// console.log(json);
-
-								// Delete or comment out the next TWO (2) lines when you are ready to proceed
-								// console.log('===\nAt this point, you\'ll need to customise the above section to id, displayName, and emails into the "profile" object.\n===');
-								// return done(new Error('Congrats! So far so good -- please see server log for details'));
-
-								done(null, profile);
-							} catch(e) {
-								done(e);
-							}
-						});
-					};
-				}
+							done(null, profile);
+						} catch(e) {
+							done(e);
+						}
+					});
+				};
+				
 
 				passport.use('Generic OAuth', new passportOAuth(opts, function(token, secret, profile, done) {
 					OAuth.login(profile.id, profile.displayName, profile.emails[0].value, function(err, user) {
--- templates/admin/plugins/sso-oauth.tpl
+++ templates/admin/plugins/sso-oauth.tpl
@@ -4,78 +4,22 @@
 <form class="sso-oauth-settings">
 	<div class="alert alert-warning">
 		<p>
-			Please refer to your OAuth provider&apos;s documentation for appropriate values. All fields are mandatory.
+			OAuth 1.0 use IMS-LTI
 		</p>
-		<br />
-		<select name="oauth:type" title="OAuth Strategy" class="form-control">
-			<option value="x">Disabled</option>
-			<option value="1">OAuth</option>
-			<option value="2">OAuth2</option>
-		</select>
+		
 		<hr />
 		<div class="form-group">
-			<input type="text" data-strategy="1" name="oauth:key" title="OAuth Key" class="form-control input-lg" placeholder="OAuth Key">
+			<input type="text"  name="consumer:key" title="Consumer Key" class="form-control input-lg" placeholder="Consumer Key">
 		</div>
 		<div class="form-group">
-			<input type="text" data-strategy="1" name="oauth:secret" title="OAuth Secret" class="form-control" placeholder="OAuth Secret">
+			<input type="text"  name="secret:key" title="Secret Key" class="form-control" placeholder="Secret Key">
 		</div>
 		<div class="form-group">
-			<input type="text" data-strategy="1" name="oauth:reqTokenUrl" title="Token Request URL" class="form-control" placeholder="Token Request URL">
+			<input type="text"  name="provider:id" title="Provider name" class="form-control" placeholder="Provider name">
 		</div>
-		<div class="form-group">
-			<input type="text" data-strategy="1" name="oauth:accessTokenUrl" title="Access Token URL" class="form-control" placeholder="Access Token URL">
-		</div>
-		<div class="form-group">
-			<input type="text" data-strategy="1" name="oauth:authUrl" title="Authorization URL" class="form-control" placeholder="Authorization URL">
-		</div>
-
-		<div class="form-group">
-			<input type="text" data-strategy="2" name="oauth2:id" title="Client ID" class="form-control input-lg" placeholder="Client ID">
-		</div>
-		<div class="form-group">
-			<input type="text" data-strategy="2" name="oauth2:secret" title="Client Secret" class="form-control" placeholder="Client Secret">
-		</div>
-		<div class="form-group">
-			<input type="text" data-strategy="2" name="oauth2:authUrl" title="Authorization URL" class="form-control" placeholder="Authorization URL">
-		</div>
-		<div class="form-group">
-			<input type="text" data-strategy="2" name="oauth2:tokenUrl" title="Token URL" class="form-control" placeholder="Token URL">
-		</div>
-
-		<div class="form-group">
-			<input type="text" name="oauth:userProfileUrl" title="User Profile URL" class="form-control" placeholder="User Profile URL">
-		</div>
+		
 	</div>
 </form>
 
 <button class="btn btn-lg btn-primary" type="button" id="save">Save</button>
 
-<script>
-	require(['settings'], function(Settings) {
-		Settings.load('sso-oauth', $('.sso-oauth-settings'), function() {
-			var	OAuthType = $('[name="oauth:type"]').val();
-			toggleFields(OAuthType);
-		});
-
-		$('#save').on('click', function() {
-			Settings.save('sso-oauth', $('.sso-oauth-settings'));
-		});
-	});
-
-	var	toggleFields = function(value) {
-		if (value === '1') {
-			$('[data-strategy="2"]').hide();
-			$('[data-strategy="1"]').show();
-		} else if (value === '2') {
-			$('[data-strategy="1"]').hide();
-			$('[data-strategy="2"]').show();
-		} else {
-			$('[data-strategy]').hide();
-		}
-	}
-
-	toggleFields(false);
-	$('[name="oauth:type"]').on('change', function() {
-		toggleFields(this.value);
-	})
-</script>
